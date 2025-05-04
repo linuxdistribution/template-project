3d29dce76f1f93a14b5b4afc1962226bdd9e3597
linuxdistribution <linuxdistribution@duck.com>
2025-05-04 13:09:50 +0200

    Added tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,15 @@ tasks {
     }
 }
 
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    defaultCharacterEncoding = "UTF-8"
 }
 
